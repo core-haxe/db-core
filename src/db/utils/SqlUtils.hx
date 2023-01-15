c4bd4b3c8b9efef43023db031a621a50317f74d8
Ian Harrigan <ianharrigan@hotmail.com>
2023-01-15 21:54:57 +0100

    better join sql (alias field list based on schema)

--- src/db/utils/SqlUtils.hx
+++ src/db/utils/SqlUtils.hx
@@ -20,9 +20,16 @@ class SqlUtils {
         return sql;
     }
 
-    public static function buildSelect(table:ITable, query:QueryExpr = null, limit:Null<Int> = null, values:Array<Any> = null, relationships:RelationshipDefinitions = null, relationshipExclusions:Array<String> = null):String {
-        var sql = 'SELECT * FROM ${table.name}';
-        sql += addJoins(table.name, relationships, false, null, relationshipExclusions);
+    public static function buildSelect(table:ITable, query:QueryExpr = null, limit:Null<Int> = null, values:Array<Any> = null, relationships:RelationshipDefinitions = null, relationshipExclusions:Array<String> = null, databaseSchema:DatabaseSchema = null):String {
+        var fieldAliases = [];
+        var sqlJoin = addJoins(table.name, relationships, databaseSchema, fieldAliases, false, null, relationshipExclusions);
+
+        var fieldList = '*';
+        if (fieldAliases.length > 0) {
+            fieldList = fieldAliases.join(", ");
+        }
+        var sql = 'SELECT ${fieldList} FROM ${table.name}';
+        sql += sqlJoin;
 
         if (query != null) {
             sql += '\nWHERE (${Query.queryExprToSql(query, values)})';
@@ -34,7 +41,7 @@ class SqlUtils {
         return sql;
     }
 
-    private static function addJoins(tableName:String, relationships:RelationshipDefinitions, invert:Bool = false, counters:Map<String, Int> = null, exclusions:Array<String> = null) {
+    private static function addJoins(tableName:String, relationships:RelationshipDefinitions, databaseSchema:DatabaseSchema, fieldAliases:Array<String>, invert:Bool = false, counters:Map<String, Int> = null, exclusions:Array<String> = null) {
         if (relationships == null) {
             return "";
         }
@@ -47,10 +54,30 @@ class SqlUtils {
             return "";
         }
 
+        var alwaysAlias:Bool = true; // might want to make this configurable somehow, output is actually maybe more sensible that "field_1", instead it can be "table.join_table.field"
+
         var sql = "";
         if (relationships != null) {
             var localExclusions = exclusions.copy();
             exclusions.push(tableName);
+
+            if (databaseSchema != null) {
+                var tableSchema = databaseSchema.findTable(tableName);
+                if (!alwaysAlias) {
+                    for (tableColumn in tableSchema.columns) {
+                        var fieldAlias = '${tableName}.${tableColumn.name}';
+                        if (!fieldAliases.contains(fieldAlias)) {
+                            fieldAliases.push(fieldAlias);
+                        }
+                    }
+                } else {
+                    for (tableColumn in tableSchema.columns) {
+                        var fieldAlias = '${tableName}.${tableColumn.name}';
+                        var fieldAs = '${fieldAlias} as "${fieldAlias}"';
+                        fieldAliases.push(fieldAs);
+                    }
+                }
+            }
 
             var tableRelationships = relationships.get(tableName);
             if (tableRelationships != null) {
@@ -81,8 +108,41 @@ class SqlUtils {
                     var sourceTableAlias = sourceTable;
                     if (counters.exists(sourceTable)) {
                         table2Alias += counters.get(sourceTable);
+                        sourceTableAlias += counters.get(sourceTable);
                         as = ' AS ${table2Alias}';
                     }
+
+                    if (databaseSchema != null) {
+                        var tableSchema = databaseSchema.findTable(sourceTable);
+                        if (!alwaysAlias) {
+                            for (tableColumn in tableSchema.columns) {
+                                var field = '${sourceTable}.${tableColumn.name}';
+                                if (counters.exists(field)) {
+                                    var fieldAlias = '${sourceTableAlias}.${tableColumn.name}';
+                                    //var fieldAs = '${fieldAlias} as "${tableColumn.name}_${counters.get(field)}"';
+                                    var fieldAs = '${fieldAlias} as "${sourceTable}.${tableColumn.name}"';
+                                    if (!fieldAliases.contains(fieldAs)) {
+                                        fieldAliases.push(fieldAs);
+                                    }
+                                } else {
+                                    counters.set(field, 1);
+                                    if (!fieldAliases.contains(field)) {
+                                        fieldAliases.push(field);
+                                    }
+                                }
+                            }
+                        } else {
+                            for (tableColumn in tableSchema.columns) {
+                                var fieldAlias = '${sourceTableAlias}.${tableColumn.name}';
+                                var fieldAs = '${fieldAlias} as "${sourceTable}.${tableColumn.name}"';
+                                if (tableName != sourceTable) {
+                                    fieldAs = '${fieldAlias} as "${tableName}.${sourceTable}.${tableColumn.name}"';
+                                }
+                                fieldAliases.push(fieldAs);
+                            }
+                        }
+                    }
+        
                     sql += '\n    INNER JOIN ${sourceTable}${as} ON ${table2Alias}.${field2} = ${table1Alias}.${field1}';
                     if (!counters.exists(sourceTable)) {
                         counters.set(sourceTable, 1);
@@ -109,7 +169,7 @@ class SqlUtils {
 
                 if (additionalTables.length > 0) {
                     for (a in additionalTables) {
-                        sql += addJoins(a, relationships, true, counters, exclusions);
+                        sql += addJoins(a, relationships, databaseSchema, fieldAliases, true, counters, exclusions);
                     }
                 }
     
