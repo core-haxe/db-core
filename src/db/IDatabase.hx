e2e85c3a225fa7e1885f21521bee01209b443120
Ian Harrigan <ianharrigan@hotmail.com>
2023-10-21 08:35:55 +0200

    create / delete database

--- src/db/IDatabase.hx
+++ src/db/IDatabase.hx
@@ -15,6 +15,9 @@ interface IDatabase {
     public function connect():Promise<DatabaseResult<Bool>>;
     public function disconnect():Promise<DatabaseResult<Bool>>;
 
+    public function create():Promise<DatabaseResult<IDatabase>>;
+    public function delete():Promise<DatabaseResult<Bool>>;
+
     public function table(name:String):Promise<DatabaseResult<ITable>>;
     public function createTable(name:String, columns:Array<ColumnDefinition>):Promise<DatabaseResult<ITable>>;
     public function deleteTable(name:String):Promise<DatabaseResult<Bool>>;
