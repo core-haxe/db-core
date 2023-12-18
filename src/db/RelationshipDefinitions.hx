f648a69f476194ca52f85356f003eacf7d1441a3
Ian Harrigan <ianharrigan@hotmail.com>
2023-12-18 17:30:49 +0100

    dont add duplicate relationships

--- src/db/RelationshipDefinitions.hx
+++ src/db/RelationshipDefinitions.hx
@@ -11,6 +11,9 @@ class RelationshipDefinitions {
     public var relationships:Map<String, String>;
 
     public function add(field1:String, field2:String) {
+        if (has(field1, field2)) {
+            return;
+        }
         var parts1 = field1.split(".");
         var parts2 = field2.split(".");
         var table1 = parts1.shift();
@@ -30,6 +33,28 @@ class RelationshipDefinitions {
             field1: field1,
             field2: field2
         });
+    }
+
+    public function has(field1:String, field2:String):Bool {
+        var parts1 = field1.split(".");
+        var parts2 = field2.split(".");
+        var table1 = parts1.shift();
+        var table2 = parts2.shift();
+        var field1 = parts1.join(".");
+        var field2 = parts2.join(".");
+
+        var array = _defs.get(table1);
+        if (array == null) {
+            return false;
+        }
+
+        for (item in array) {
+            if (item.table1 == table1 && item.table2 == table2 && item.field1 == field1 && item.field2 == field2) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public function get(table:String):Array<RelationshipDefinition> {
