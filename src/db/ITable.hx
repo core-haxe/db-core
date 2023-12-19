61ac7772167f004027d7c90be773a49a607e16f3
Ian Harrigan <ianharrigan@hotmail.com>
2023-12-19 14:54:31 +0100

    move to recordsets

--- src/db/ITable.hx
+++ src/db/ITable.hx
@@ -11,16 +11,16 @@ interface ITable {
     public function schema():Promise<DatabaseResult<TableSchema>>;
     public function applySchema(newSchema:TableSchema):Promise<DatabaseResult<TableSchema>>;
 
-    public function all():Promise<DatabaseResult<Array<Record>>>;
-    public function page(pageIndex:Int, pageSize:Int = 100, query:QueryExpr = null, allowRelationships:Bool = true):Promise<DatabaseResult<Array<Record>>>;
+    public function all():Promise<DatabaseResult<RecordSet>>;
+    public function page(pageIndex:Int, pageSize:Int = 100, query:QueryExpr = null, allowRelationships:Bool = true):Promise<DatabaseResult<RecordSet>>;
     public function add(record:Record):Promise<DatabaseResult<Record>>;
-    public function addAll(records:Array<Record>):Promise<DatabaseResult<Array<Record>>>;
+    public function addAll(records:RecordSet):Promise<DatabaseResult<RecordSet>>;
     public function delete(record:Record):Promise<DatabaseResult<Record>>;
     public function deleteAll(query:QueryExpr = null):Promise<DatabaseResult<Bool>>;
     public function update(query:QueryExpr, record:Record):Promise<DatabaseResult<Record>>;
-    public function find(query:QueryExpr, allowRelationships:Bool = true):Promise<DatabaseResult<Array<Record>>>;
+    public function find(query:QueryExpr, allowRelationships:Bool = true):Promise<DatabaseResult<RecordSet>>;
     public function findOne(query:QueryExpr, allowRelationships:Bool = true):Promise<DatabaseResult<Record>>;
-    public function findUnique(columnName:String, query:QueryExpr = null, allowRelationships:Bool = true):Promise<DatabaseResult<Array<Record>>>;
+    public function findUnique(columnName:String, query:QueryExpr = null, allowRelationships:Bool = true):Promise<DatabaseResult<RecordSet>>;
     public function count(query:QueryExpr = null):Promise<DatabaseResult<Int>>;
 
     public function addColumn(column:ColumnDefinition):Promise<DatabaseResult<Bool>>;
---
+++ src/db/RecordSet.hx
@@ -0,0 +1,78 @@
+package db;
+
+@:forward
+@:forward.new
+@:forwardStatics
+abstract RecordSet(RecordSetImpl) {
+    @:arrayAccess
+    private inline function get(index:Int) {
+        return @:privateAccess this.records[index];
+    }
+
+    @:arrayAccess
+    private inline function set(index:Int, value:Record):Record {
+        @:privateAccess this.records[index] = value;
+        return value;
+    }
+
+    @:from
+    private static inline function fromArray(records:Array<Record>) {
+        return new RecordSet(records);
+    }
+
+    @:to 
+    private inline function toArray():Array<Record> {
+        return @:privateAccess this.records;
+    }
+}
+
+private class RecordSetImpl {
+    private var records:Array<Record> = null;
+
+    public function new(records:Array<Record> = null) {
+        this.records = records;
+    }
+
+    @:noCompletion
+    public function iterator():RecordSetIterator {
+        return new RecordSetIterator(records);
+    }
+
+    public var length(get, null):Int;
+    private function get_length():Int {
+        if (records == null) {
+            return 0;
+        }
+        return records.length;
+    }
+
+    public function push(record:Record) {
+        if (records == null) {
+            records = [];
+        }
+        records.push(record);
+    }
+}
+
+private class RecordSetIterator {
+    private var records:Array<Record> = null;
+    private var pos:Int = 0;
+
+    public function new(records:Array<Record> = null) {
+        this.records = records;
+        this.pos = 0;
+    }
+
+    public function hasNext():Bool {
+        if (records == null) {
+            return false;
+        }
+        return pos < records.length;
+    }
+
+    public function next():Record {
+        var r = records[pos];
+        pos++;
+        return r;
+    }
+}
