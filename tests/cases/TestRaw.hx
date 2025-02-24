0452abcfec5a280df69039758065fb2062596af6
Ian Harrigan <ianharrigan@hotmail.com>
2025-02-24 09:22:51 +0100

    join unit test

--- tests/cases/TestRaw.hx
+++ tests/cases/TestRaw.hx
@@ -64,4 +64,36 @@ class TestRaw implements ITest {
             trace("error", error);
         });
     }
+
+    function testBasicRawQuery_Join(async:Async) {
+        db.table("Person").then(result -> {
+            return result.table.find(raw("
+                SELECT * FROM Person
+                INNER JOIN Icon ON Person.iconId = Icon.IconId  
+                WHERE personId = 1 or personId = 3;            
+            "));
+        }).then(result -> {
+            Assert.equals(2, result.data.length);
+
+            Assert.equals(1, result.data[0].field("personId"));
+            Assert.equals("Ian", result.data[0].field("firstName"));
+            Assert.equals("Harrigan", result.data[0].field("lastName"));
+            Assert.equals(1, result.data[0].field("iconId"));
+            Assert.equals(111.222, result.data[0].field("hourlyRate"));
+            Assert.equals(1, result.data[0].field("iconId"));
+            Assert.equals("/somepath/icon1.png", result.data[0].field("path"));
+
+            Assert.equals(3, result.data[1].field("personId"));
+            Assert.equals("Tim", result.data[1].field("firstName"));
+            Assert.equals("Mallot", result.data[1].field("lastName"));
+            Assert.equals(2, result.data[1].field("iconId"));
+            Assert.equals(555.666, result.data[1].field("hourlyRate"));
+            Assert.equals(2, result.data[1].field("iconId"));
+            Assert.equals("/somepath/icon2.png", result.data[1].field("path"));
+            
+            async.done();
+        }, error -> {
+            trace("error", error);
+        });
+    }
 }
